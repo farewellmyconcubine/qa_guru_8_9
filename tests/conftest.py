b33c56091884ae99831c03158f5070c624ba16d5
Vadzim Belashtsentau <112775224+farewellmyconcubine@users.noreply.github.com>
2023-10-12 12:44:18 +0300

    Update conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,7 +3,7 @@ from selene.support.shared import browser
 
 
 @pytest.fixture(autouse=True)
-def init_settings(request):
+def init_settings():
     browser.config.timeout = 3
     browser.config.window_width = 1280
     browser.config.window_height = 720
